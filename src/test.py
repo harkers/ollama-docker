c560a063d0f0d16777720c25366e605cf82d8c7c
sh@rker.dev <stuharker@gmail.com>
2024-10-21 13:29:11 +0100

    loaded ip of server

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler 
                                  
 llm = Ollama(
-    base_url="http://localhost:11434",
+    base_url="http://l92.168.10.60:11434",
     model="llama2-uncensored", 
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
 
